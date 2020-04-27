786857ee55a4c96c15b606b33f36adcb5d93de2d
phest0 <jpcid@hotmail.fr>
2020-04-27 18:38:53 +0200

    feature: hide navbar on scroll

--- public/index.php
+++ public/index.php
@@ -124,7 +124,7 @@ $items = array($setup1, $setup2, $setup3, $setup4, $setup5, $setup6);
 <body>
 
     <!-- Navbar -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar" id="scrollEffectNavbar">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar banner" id="scrollEffectNavbar">
         <div class="container-fluid">
 
             <!-- Brand -->
@@ -229,10 +229,10 @@ $items = array($setup1, $setup2, $setup3, $setup4, $setup5, $setup6);
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-    <script src="./js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
